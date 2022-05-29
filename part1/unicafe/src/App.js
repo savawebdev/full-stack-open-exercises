63b1046fec2e33a871183365029fb829f066ff67
savawedev <andrei.sava.dev@gmail.com>
2022-05-29 20:57:06 +0300

    Completed exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -17,6 +17,10 @@ const App = () => {
     setBad(bad + 1);
   };
 
+  const all = good + neutral + bad;
+  const average = (good + bad * -1) / all;
+  const positive = (good / all) * 100;
+
   return (
     <div>
       <h1>Give feedback</h1>
@@ -29,6 +33,9 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive}%</p>
     </div>
   );
 };
