787b680ebd0fb96f73384757c94735cdb0dc711e
savawedev <andrei.sava.dev@gmail.com>
2022-05-30 14:53:44 +0300

    Completed exercise 2.2

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -2,11 +2,15 @@ import React from "react";
 import Part from "./Part";
 
 const Content = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, cur) => sum + cur.exercises, 0);
+
   return (
     <div>
       {parts.map((part) => (
         <Part key={part.id} part={part} />
       ))}
+
+      <strong>total of {totalExercises} exercises</strong>
     </div>
   );
 };
