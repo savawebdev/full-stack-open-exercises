9e2aa76b9af886b8c477d8f6f6ead1c55d42fa76
savawedev <andrei.sava.dev@gmail.com>
2022-05-30 14:58:31 +0300

    Completed exercises 2.3, 2.4 and 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,34 +1,58 @@
 import Course from "./components/Course";
 
-const Total = (props) => <p>Number of exercises {props.total}</p>;
-
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half stack application development",
-    parts: [
-      {
-        name: "Fundamentales of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  //prettier-ignore
-  const totalExercises = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises;
-
-  return <Course course={course} />;
+  return (
+    <div>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </div>
+  );
 };
 
 export default App;
