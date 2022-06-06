88856a5544313114c112ac91c20df79055fae8f8
savawedev <andrei.sava.dev@gmail.com>
2022-06-06 10:28:27 +0300

    Completed exercise 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,18 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 app.get("/info", (req, res) => {
   const totalPersons = persons.length;
   const date = new Date();
