13dcf5f09c6722f9f1fcfbccf503713576fc6687
savawedev <andrei.sava.dev@gmail.com>
2022-06-06 15:49:52 +0300

    Completed exercise 3.17

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -80,6 +80,21 @@ app.get("/api/persons/:id", (req, res, next) => {
     .catch((err) => next(err));
 });
 
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((err) => next(err));
+});
+
 app.delete("/api/persons/:id", (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then((result) => {
