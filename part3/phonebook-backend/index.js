8df8b6a843f7330af432b04d8fa6404a1dbf287b
savawedev <andrei.sava.dev@gmail.com>
2022-06-06 15:42:55 +0300

    Completed exercise 3.16

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,29 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const Person = require("./models/person");
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+const { response } = require("express");
 
 const app = express();
 
@@ -39,6 +17,18 @@ app.use(
 );
 app.use(cors());
 app.use(express.static("build"));
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const generateId = () => {
   const id = Math.floor(Math.random() * 100000);
@@ -78,18 +68,24 @@ app.post("/api/persons", (req, res) => {
   });
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  Person.findById(req.params.id).then((person) => {
-    res.json(person);
-  });
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((err) => next(err));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then((result) => {
       res.status(204).end();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => next(err));
 });
 
 app.get("/info", (req, res) => {
