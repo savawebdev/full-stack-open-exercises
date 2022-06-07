402e0829702bd28d2b868f73aeecd33720f2c759
savawedev <andrei.sava.dev@gmail.com>
2022-06-07 12:08:39 +0300

    Completed exercise 4.4

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -1,3 +1,10 @@
 const dummy = (blogs) => 1;
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  const likes = blogs.map((blog) => blog.likes);
+  const likesSum = likes.reduce((sum, cur) => sum + cur, 0);
+
+  return likesSum;
+};
+
+module.exports = { dummy, totalLikes };
